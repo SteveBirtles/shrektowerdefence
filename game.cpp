d2672629ba2b7839cdd54b683ed2c2d3c980ef6c
Steve Birtles <sbirtles@farnborough.ac.uk>
2021-12-06 13:51:54 +0000

    End of lesson 1

--- game.cpp
+++ game.cpp
@@ -1,9 +1,4 @@
 #define OLC_PGE_APPLICATION
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <vector>
-
 #include "olcPixelGameEngine.h"
 
 const int WINDOW_WIDTH = 1280;
@@ -11,213 +6,57 @@ const int WINDOW_HEIGHT = 720;
 
 const int TILE_SIZE = 32;
 const int TILE_COUNT = 26;
-olc::Sprite *tileSprites[TILE_COUNT];
-olc::Decal *tileDecals[TILE_COUNT];
-const std::string tileFiles[TILE_COUNT]{
-    "bush.png",   "FarFarAway.png",  "Grass1.png", "Grass2.png", "Grass3.png",
-    "Grass4.png", "Grass5.png",      "Grass6.png", "House.png",  "Marsh.png",
-    "Mud.png",    "Onion_field.png", "soil2.png",  "soil.png",   "stone.png",
-    "swamp.png",  "Toilet.png",      "tree.png",   "Water2.png", "Water3.png",
-    "Water4.png", "Water5.png",      "Water6.png", "Water7.png", "Water8.png",
-    "Water.png",
-};
-
-void loadTileGraphics() {
-  for (auto i = 0; i < TILE_COUNT; i++) {
-    tileSprites[i] = new olc::Sprite("./Tiles/" + tileFiles[i]);
-    tileDecals[i] = new olc::Decal(tileSprites[i]);
-  }
-}
 
 const int MAP_WIDTH = 40;
 const int MAP_HEIGHT = 20;
-int map[MAP_WIDTH][MAP_HEIGHT];
-
-const int TOWER_SIZE = 32;
-const int TOWER_COUNT = 7;
-olc::Sprite *towerSprites[TOWER_COUNT];
-olc::Decal *towerDecals[TOWER_COUNT];
-const std::string towerFiles[TOWER_COUNT]{
-    "donkey.png",        "Dragon.png",          "gingerbreadSpriteSheet.png",
-    "Puss_in_boots.png", "rumpelstiltskin.png", "shrek.png",
-    "smashMouth.png",
-};
-
-struct Tower {
-  int x;
-  int y;
-  int towerNumber;
-};
-std::vector<Tower *> towers;
-
-void loadTowerGraphics() {
-  for (auto i = 0; i < TOWER_COUNT; i++) {
-    towerSprites[i] = new olc::Sprite("./Towers/" + towerFiles[i]);
-    towerDecals[i] = new olc::Decal(towerSprites[i]);
-  }
-}
-
-void addRandomTowers() {
-  for (auto i = 0; i < 40; i++) {
-    if (towers.size() == MAP_WIDTH * MAP_HEIGHT) break;
-    int x, y;
-    auto repeated = true;
-    while (repeated) {
-      x = rand() % MAP_WIDTH;
-      y = rand() % MAP_HEIGHT;
-      repeated = false;
-      for (auto tower : towers) {
-        if (tower->x == x && tower->y == y) {
-          repeated = true;
-        }
-      }
-    }
-    towers.push_back(new Tower{x, y, rand() % TOWER_COUNT});
-  }
-}
-
-void resetTowers() {
-  for (auto tower : towers) {
-    delete tower;
-  }
-  towers.clear();
-}
-
-void resetMap() {
-  for (auto x = 0; x < MAP_WIDTH; x++) {
-    for (auto y = 0; y < MAP_HEIGHT; y++) {
-      map[x][y] = 10;  // Mud.png
-    }
-  }
-}
-
-void saveMap() {
-  std::ofstream mapFile;
-  mapFile.open("map.csv");
-  mapFile << "MAP_WIDTH," << std::to_string(MAP_WIDTH) << std::endl;
-  mapFile << "MAP_HEIGHT," << std::to_string(MAP_HEIGHT) << std::endl;
-  for (auto y = 0; y < MAP_HEIGHT; y++) {
-    mapFile << "MAP_ROW,";
-    for (auto x = 0; x < MAP_WIDTH; x++) {
-      mapFile << std::to_string(map[x][y]);
-      if (x != MAP_WIDTH - 1) {
-        mapFile << ",";
-      } else {
-        mapFile << std::endl;
-      }
-    }
-  }
-  for (auto t = 0; t < towers.size(); t++) {
-    mapFile << "TOWER," << std::to_string(towers[t]->x) << ","
-            << std::to_string(towers[t]->y) << ","
-            << std::to_string(towers[t]->towerNumber) << std::endl;
-  }
-  mapFile.close();
-}
-
-void loadMap() {
-  std::string line;
-  std::ifstream mapFile("map.csv");
-
-  resetTowers();
-
-  if (mapFile.is_open()) {
-    int x = 0;
-    int y = -1;
-    std::string bit;
-    enum RowType { UNKNOWN, WIDTH_CHECK, HEIGHT_CHECK, MAP_ROW, TOWER };
-    Tower *newTower;
-
-    while (std::getline(mapFile, line)) {
-      try {
-        std::size_t commaPosition = line.find(",");
-        std::size_t lastCommaPosition = -1;
-        bool done = false;
-        auto rowType = UNKNOWN;
-
-        while (!done) {
-          if (commaPosition == std::string::npos) {
-            bit = line.substr(lastCommaPosition + 1);
-            done = true;
-          } else {
-            bit = line.substr(lastCommaPosition + 1,
-                              commaPosition - lastCommaPosition - 1);
-            lastCommaPosition = commaPosition;
-            commaPosition = line.find(",", lastCommaPosition + 1);
-          }
-
-          if (bit == "MAP_WIDTH") {
-            rowType = WIDTH_CHECK;
-          } else if (bit == "MAP_HEIGHT") {
-            rowType = HEIGHT_CHECK;
-          } else if (bit == "MAP_ROW") {
-            rowType = MAP_ROW;
-            x = 0;
-            y++;
-          } else if (bit == "TOWER") {
-            rowType = TOWER;
-            newTower = new Tower();
-            x = 0;
-          } else {
-            switch (rowType) {
-              case WIDTH_CHECK:
-                if (std::stoi(bit) != MAP_WIDTH) {
-                  std::cerr << "Tried to load a map with invalid width."
-                            << std::endl;
-                  return;
-                }
-                break;
-              case HEIGHT_CHECK:
-                if (std::stoi(bit) != MAP_HEIGHT) {
-                  std::cerr << "Tried to load a map with invalid height."
-                            << std::endl;
-                  return;
-                }
-                break;
-              case MAP_ROW:
-                map[x][y] = std::stoi(bit);
-                x++;
-                break;
-              case TOWER:
-                if (x == 0) {
-                  newTower->x = std::stoi(bit);
-                } else if (x == 1) {
-                  newTower->y = std::stoi(bit);
-                } else if (x == 2) {
-                  newTower->towerNumber = std::stoi(bit);
-                  towers.push_back(newTower);
-                }
-                x++;
-            }
-          }
-        }
-      } catch (int e) {
-        std::cout << "An error occured: " << e << std::endl;
-      }
-    }
-  }
-}
-
-const int ANIMATION_RATE = 6;
 
 class Game : public olc::PixelGameEngine {
  public:
-  Game() { sAppName = "Shrek Tower Defence"; }
+  Game() { sAppName = "Insert Game Name Here"; }
 
  private:
   float timer = 0;
   int frames = 0;
   int fps;
+
+  olc::Sprite* tileSprites[TILE_COUNT];
+  olc::Decal* tileDecals[TILE_COUNT];
+
   int currentTile = 2;  // Grass1.png
-  float animationTimer = 0;
+
+  const std::string tileFiles[TILE_COUNT]{
+      "bush.png",   "FarFarAway.png",  "Grass1.png", "Grass2.png", "Grass3.png",
+      "Grass4.png", "Grass5.png",      "Grass6.png", "House.png",  "Marsh.png",
+      "Mud.png",    "Onion_field.png", "soil2.png",  "soil.png",   "stone.png",
+      "swamp.png",  "Toilet.png",      "tree.png",   "Water2.png", "Water3.png",
+      "Water4.png", "Water5.png",      "Water6.png", "Water7.png", "Water8.png",
+      "Water.png",
+  };
+
+  int map[MAP_WIDTH][MAP_HEIGHT];
+
+  void loadTileGraphics() {
+    for (auto i = 0; i < TILE_COUNT; i++) {
+      tileSprites[i] = new olc::Sprite("./Tiles/" + tileFiles[i]);
+      tileDecals[i] = new olc::Decal(tileSprites[i]);
+    }
+  }
+
+  void resetMap() {
+    for (auto x = 0; x < MAP_WIDTH; x++) {
+      for (auto y = 0; y < MAP_HEIGHT; y++) {
+        map[x][y] = 10;  // Mud.png
+      }
+    }
+  }
 
  public:
   bool OnUserCreate() override {
+    /*
+      Load resources here
+    */
     loadTileGraphics();
-    loadTowerGraphics();
     resetMap();
-    resetTowers();
-    addRandomTowers();
 
     return true;
   }
@@ -230,9 +69,9 @@ class Game : public olc::PixelGameEngine {
       frames = 0;
       timer -= 1;
     }
-    animationTimer += fElapsedTime;
 
     inputs();
+    processes();
     outputs();
 
     if (GetKey(olc::Key::ESCAPE).bPressed) {
@@ -243,49 +82,47 @@ class Game : public olc::PixelGameEngine {
   }
 
   void inputs() {
+    /*
+      Game controls goes here
+    */
+
     if (GetKey(olc::Key::DOWN).bPressed) {
-      currentTile = (currentTile + 1) % TILE_COUNT;
+      currentTile++;
+      if (currentTile > TILE_COUNT - 1) currentTile = 0;
     }
 
     if (GetKey(olc::Key::UP).bPressed) {
-      currentTile = (currentTile + TILE_COUNT - 1) % TILE_COUNT;
+      currentTile--;
+      if (currentTile < 0) currentTile = TILE_COUNT - 1;
     }
 
-    if (GetKey(olc::Key::N).bPressed) {
-      resetMap();
-      resetTowers();
-    }
+    auto x = GetMouseX() / TILE_SIZE;
+    auto y = GetMouseY() / TILE_SIZE;
 
-    if (GetKey(olc::Key::S).bPressed) {
-      saveMap();
-    }
+    if (x < 0) x = 0;
+    if (x > MAP_WIDTH - 1) x = MAP_WIDTH - 1;
+    if (y < 0) y = 0;
+    if (y > MAP_HEIGHT - 1) y = MAP_HEIGHT - 1;
 
-    if (GetKey(olc::Key::L).bPressed) {
-      loadMap();
+    if (GetMouse(0).bHeld) {
+      map[x][y] = currentTile;
+    } else if (GetMouse(1).bHeld) {
+      currentTile = map[x][y];
     }
+  }
 
-    if (GetKey(olc::Key::T).bPressed) {
-      addRandomTowers();
-    }
-
-    if (GetMouse(0).bHeld || GetMouse(1).bHeld) {
-      auto x = GetMouseX() / TILE_SIZE;
-      auto y = GetMouseY() / TILE_SIZE;
-      if (x < 0) x = 0;
-      if (x > MAP_WIDTH - 1) x = MAP_WIDTH - 1;
-      if (y < 0) y = 0;
-      if (y > MAP_HEIGHT - 1) y = MAP_HEIGHT - 1;
-      if (GetMouse(0).bHeld) {
-        map[x][y] = currentTile;
-      } else {
-        currentTile = map[x][y];
-      }
-    }
+  void processes() {
+    /*
+      Game logic goes here
+    */
   }
 
   void outputs() {
     SetPixelMode(olc::Pixel::NORMAL);
 
+    /*
+      Game graphics drawn here
+    */
     for (auto x = 0; x < MAP_WIDTH; x++) {
       for (auto y = 0; y < MAP_HEIGHT; y++) {
         auto index = map[x][y];
@@ -297,26 +134,18 @@ class Game : public olc::PixelGameEngine {
     auto position = olc::vi2d{20, WINDOW_HEIGHT - TILE_SIZE - 20};
     DrawDecal(position, tileDecals[currentTile]);
 
-    SetPixelMode(olc::Pixel::MASK);
-
-    for (auto tower : towers) {
-      auto index = tower->towerNumber;
-      auto frameCount = towerSprites[index]->width / TOWER_SIZE;
-      auto frame = int(animationTimer * ANIMATION_RATE) % frameCount;
-      auto position = olc::vi2d{tower->x * TILE_SIZE, tower->y * TILE_SIZE};
-      auto size = olc::vi2d{TOWER_SIZE, TOWER_SIZE};
-      auto offset = olc::vi2d{TOWER_SIZE * frame, 0};
-      DrawPartialDecal(position, size, towerDecals[index], offset, size);
-    }
-
     if (fps > 0) {
-      auto position = olc::vi2d(WINDOW_WIDTH - 70, WINDOW_HEIGHT - 70);
-      DrawStringDecal(position, "FPS " + std::to_string(fps));
+      auto fpsPosition = olc::vi2d(WINDOW_WIDTH - 70, WINDOW_HEIGHT - 70);
+      DrawStringDecal(fpsPosition, "FPS " + std::to_string(fps));
     }
   }
 
   bool OnUserDestroy() override {
-    std::cout << "Closing application" << std::endl;
+    std::cout << "Closing game" << std::endl;
+    for (auto i = 0; i < TILE_COUNT; i++) {
+      delete tileSprites[i];
+      delete tileDecals[i];
+    }
     return true;
   }
 };
